3669501d26c2969faec78dd46e3229817992d685
Marc MICHIELI <zyx1@free.fr>
2018-11-06 00:24:15 +0100

    initial commit

---
+++ src/main/java/com/mmi/art/incx/incxmicroservices/IncxMicroservicesApplication.java
@@ -0,0 +1,12 @@
+package com.mmi.art.incx.incxmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncxMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncxMicroservicesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mmi/art/incx/incxmicroservices/IncxMicroservicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mmi.art.incx.incxmicroservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IncxMicroservicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
